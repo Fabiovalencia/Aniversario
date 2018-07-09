d4bed52a6fad4c1b5b3107b61ac3d15097e6fe4f
Fabio <Fabio@DESKTOP-0KP33FI>
2018-07-08 19:20:47 -0500

    Preparación para el Login XAML y la instancia para llamarlo

--- Aniversario/Aniversario/Aniversario/App.xaml.cs
+++ Aniversario/Aniversario/Aniversario/App.xaml.cs
@@ -13,7 +13,7 @@ namespace Aniversario
 		{
 			InitializeComponent();
 
-			MainPage = new LoginPage();
+			MainPage = new NavigationPage ( new LoginPage());
 		}
 
 		protected override void OnStart ()
