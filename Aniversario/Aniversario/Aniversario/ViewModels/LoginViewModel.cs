13f952285fd2b1accb26edc2771269cc573e8da7
Fabio <FabioHPNoVirtualMachine>
2018-07-11 09:47:12 -0500

    Agregar BaseViewModel como clase para meter menos codigo en LoginViewModel

--- Aniversario/Aniversario/Aniversario/ViewModels/LoginViewModel.cs
+++ Aniversario/Aniversario/Aniversario/ViewModels/LoginViewModel.cs
@@ -5,11 +5,8 @@
     using System.Windows.Input;
     using Xamarin.Forms;
 
-    public class LoginViewModel : INotifyPropertyChanged
-    {
-        #region Events
-        public event PropertyChangedEventHandler PropertyChanged;
-        #endregion
+    public class LoginViewModel : BaseViewModel
+    {        
         #region Attributes
         private string email;
         private string password;
@@ -19,67 +16,27 @@
         #region Propierties
         public string Email
         {
-            get
-            {
-                return this.email;
-            }
-            set
-            {
-                if (this.email != value)
-                {
-                    this.email = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Email)));
+            get { return this.email; }
 
-                }
-            }
+            set { SetValue(ref this.email, value); }
         }
         public string Password
         {
-            get
-            {
-                return this.password;
-            }
-            set
-            {
-                if (this.password != value)
-                {
-                    this.password = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Password)));
+            get { return this.password; }
 
-                }
-            }
+            set { SetValue(ref this.password, value); }
         }
         public bool IsRunning
         {
-            get
-            {
-                return this.isRunning;
-            }
-            set
-            {
-                if (this.isRunning != value)
-                {
-                    this.isRunning = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsRunning)));
+            get { return this.isRunning; }
 
-                }
-            }
+            set { SetValue(ref this.isRunning, value); }
         }
         public bool IsEnabled
         {
-            get
-            {
-                return this.isEnabled;
-            }
-            set
-            {
-                if (this.isEnabled != value)
-                {
-                    this.isEnabled = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+            get { return this.isEnabled; }
 
-                }
-            }
+            set { SetValue(ref this.isEnabled, value); }
         }
         #endregion
         #region Constructors
